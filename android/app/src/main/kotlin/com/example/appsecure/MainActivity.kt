f9d6ff6a8ccca93cac688869854a0bb85fd8b2c5
Jhonatan32234 <233421@ids.upchiapas.edu.mx>
2026-05-22 21:36:34 -0600

    Cambios para practica 1

--- android/app/src/main/kotlin/com/example/appsecure/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/appsecure/MainActivity.kt
@@ -1,5 +1,13 @@
-package com.example.appsecure
+package com.example.appsecure // Mantén tu paquete original aquí
 
+import android.view.WindowManager
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Activa la bandera de seguridad para toda la aplicación
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
